ec7a2466be15afb4bc0ae3b1c24ccc26902f7c0c
your-github-username <rosenaumason@gmail.com>
2024-02-15 11:49:36 -0800

    can sort

--- sorter.js
+++ sorter.js
@@ -1,5 +1,21 @@
 tasks = [
-    {"id":1707450491497,"name":"Finish CS 361 milestone 1","due":"2024-02-12","do":"2024-02-08","done":false},
-    {"id":1707450689554,"name":"Walk the dog","due":"0001-01-01","do":"0002-02-02","done":false}
+    {"id":1707450491497,"name":"Last","due":"2024-02-12","do":"2027-02-08","done":false},
+    {"id":1707450689554,"name":"Second","due":"0001-01-01","do":"2026-02-02","done":false},
+    {"id":1707450689554,"name":"First","due":"0001-01-01","do":"2026-02-01","done":false}
 ]
 
+//print tasks before sorting
+console.log("Before sorting")
+console.log(tasks);
+
+//function to sort tasks by "do" field
+function sortByDate(a, b) {
+    const dateA = new Date(a.do);
+    const dateB = new Date(b.do);
+    return dateA - dateB;
+}
+
+//sort
+tasks.sort(sortByDate);
+console.log("After sorting")
+console.log(tasks);
